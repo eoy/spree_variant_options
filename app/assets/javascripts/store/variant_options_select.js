6c894635b038187e761038a69d17221592508a87
Niklas Schröder <niklas@milkpluschocolate.com>
2013-06-06 00:12:52 +0300

    working select boxes

---
+++ app/assets/javascripts/store/variant_options_select.js
@@ -0,0 +1,102 @@
+$.extend({
+	keys: function(obj){
+		return $.map(obj, function(el, ix) {return ix});
+	}
+});
+
+if (!Array.indexOf) Array.prototype.indexOf = function(obj) {
+	for(var i = 0; i < this.length; i++){
+		if(this[i] == obj) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+if (!Array.find_matches) Array.find_matches = function(a) {
+	var i, m = [];
+	a = a.sort();
+	i = a.length
+	while(i--) {
+		if (a[i - 1] == a[i]) {
+			m.push(a[i]);
+		}
+	}
+	if (m.length == 0) {
+		return false;
+	}
+	return m;
+}
+
+function VariantOptionsSelect(params) {
+
+	var options = params['options'];
+	var track_inventory_levels = params['track_inventory_levels'];
+	var allow_select_outofstock = params['allow_select_outofstock'];
+	var default_instock = params['default_instock'];
+
+	var variant, divs, inputs, selection = [];
+
+	function init() {
+		divs = $('#product-variants .variant-options');
+		inputs = divs.find('select');
+		inputs.on('change', update);
+		console.log(options);
+	}			
+  
+	function update() {
+		// x = data[option_type_id][option_value_id][id]
+		// selection = {"2" => 1, "4" => 1, "2" => 0}
+		selection = {};
+		$(inputs).each(function(ix, el) {
+			selection[get_type_id($(el).attr('id'))] = $(el).val();
+		});
+		console.log(selection);
+		
+		var matches = get_variants_from_selection(selection);
+		console.log(matches);
+		if(matches.length > 0) {
+			$('.price').html(matches[0].price)
+		}
+	}
+	
+	function get_type_id(str) {
+		var tmp = str.split("_");
+		return tmp[tmp.length-1];
+	}
+	
+	function get_variants_with_option(otid ,ovid) {
+		return options[otid][ovid];
+	}
+	
+	function get_variants_from_selection(selection) {
+		var candidates = {};
+		var keys = [];
+		var option_type_ids = $.keys(selection);
+		$.each(option_type_ids, function(key, otid) {
+			var ovid = selection[otid];
+			var vars = get_variants_with_option(otid,ovid);
+			$.merge(keys, $.keys(vars));
+			$.extend(candidates, vars);
+		});
+		
+		var matches = []
+		$.each(candidates, function(key, value) {
+			if(element_count(key, keys) >= option_type_ids.length) {
+				matches.push( value);
+			}
+		});
+		
+		return matches;
+	}
+	
+	function element_count(item, array) {
+	    var count = 0;
+	    $.each(array, function(i,v) { if (v === item) count++; });
+	    return count;
+	}
+
+
+	$(document).ready(init);
+
+};
--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -9,8 +9,9 @@ Spree::Variant.class_eval do
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
     { 
       :id    => self.id, 
-      :count => self.count_on_hand, 
-      :price => number_to_currency(actual_price)
+      :count => Spree::StockItem.find_all_by_variant_id(self).sum(&:count_on_hand), 
+      :price => number_to_currency(actual_price),
+			:backorderable => Spree::StockItem.where({variant_id: self.id, backorderable: true})  ? true : false
     }
   end
     
